1c1aaa1eca0984906b298fa8dd7ad4146fe7cf72
uniqweber <uniqwebersocial@gmail.com>
2025-04-10 14:11:24 +0600

    explore function params and return params

--- 03-80:20-ts-learning/mostUsed.ts
+++ 03-80:20-ts-learning/mostUsed.ts
@@ -2,3 +2,13 @@
 const names: string = "hello";
 const age: number = 12;
 const isActive: boolean = false;
+
+// type interface - typescript will it's own what is the type
+let message = "hello";
+
+// function params and return params types
+const greet = (name: string): string => {
+  return `Hello ${name}`;
+};
+
+
